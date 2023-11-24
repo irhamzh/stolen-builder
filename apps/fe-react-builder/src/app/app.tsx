c933cfc0ea4ea91e7d330ab08698b725f327e3ca
irhamzh <irhamz175@gmail.com>
2023-11-24 21:39:28 +0700

    Initial commit

---
+++ apps/fe-react-builder/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="fe-react-builder" />
+    </div>
+  );
+}
+
+export default App;
