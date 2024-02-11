84143b9a7de8b19515b6b4a07e0610d7fcb76396
Irham Zhafir <irhamz175@gmail.com>
2024-02-11 18:38:24 +0700

    Add prev modules on to the main app module

--- apps/be-nest-api/src/app/app.module.ts
+++ apps/be-nest-api/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemModule } from './modules/item/item.module';
+import { SkillTreeModule } from './modules/skill-tree/skill-tree.module';
+import { FortuneModule } from './modules/fortune/fortune.module';
+import { CharacterModule } from './modules/character/character.module';
+import { SideEffectModule } from './modules/side-effect/side-effect.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ItemModule,
+		SkillTreeModule,
+		FortuneModule,
+		CharacterModule,
+		SideEffectModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
